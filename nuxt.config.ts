ae22181a571b1b797f578a2a4aa53b7e62f48433
liting <luz.liting@gmail.com>
2023-08-06 21:56:17 +0800

    refactor(tailwindcss): Typography config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,7 @@ export default defineNuxtConfig({
   },
   tailwindcss: {
     cssPath: '~/assets/styles/tailwind.css',
+    configPath: '~/tailwind.config.ts',
   },
   colorMode: {
     preference: 'light',
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,31 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
-
+  theme: {
+    extend: {
+      // @ts-expect-error theme type
+      typography: theme => ({
+        DEFAULT: {
+          css: {
+            'blockquote p:first-of-type::before': {
+              content: '',
+            },
+            'blockquote p:last-of-type::after': {
+              content: '',
+            },
+            'code': {
+              'border-radius': '4px',
+              'padding': '4px',
+            },
+            'code::before': {
+              content: '',
+            },
+            'code::after': {
+              content: '',
+            },
+          },
+        },
+      }),
+    },
+  },
 }
