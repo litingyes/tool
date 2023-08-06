a3e33441fa495b20db62b29eb7e50e65b39482b4
liting <luz.liting@gmail.com>
2023-08-07 00:39:47 +0800

    perf(toggleLang): hover style

--- components/ToggleLang.vue
+++ components/ToggleLang.vue
@@ -9,14 +9,14 @@ const { locale } = useI18n()
     <template #panel>
       <ul class="w-20 text-sm">
         <li
-          class="cursor-pointer p-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700"
-          :class="[locale === 'en' && 'text-primary-500 dark:text-primary-400']" @click="locale = 'en'"
+          class="cursor-pointer px-2 py-1 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700"
+          :class="[locale === 'en' ? 'text-primary-500 dark:text-primary-400' : 'hover:text-primary-600 hover:dark:text-primary-300']" @click="locale = 'en'"
         >
           English
         </li>
         <li
-          class="cursor-pointer p-2 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700"
-          :class="[locale === 'zh' && 'text-primary-500 dark:text-primary-400']" @click="locale = 'zh'"
+          class="cursor-pointer px-2 py-1 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700"
+          :class="[locale === 'zh' ? 'text-primary-500 dark:text-primary-400' : 'hover:text-primary-600 hover:dark:text-primary-300']" @click="locale = 'zh'"
         >
           简体中文
         </li>
