aa628e9b60eb8d0bb8b84912917bb5b9304aaee7
liting <luz.liting@gmail.com>
2023-08-06 08:59:43 +0800

    chore(tailwindcss): rm invalid config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,6 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
-  content: [
-    './pages/**/*.vue',
-    './components/**/*.vue',
-  ],
   theme: {
     fontFamily: {
       smiley: ['SmileySans'],
