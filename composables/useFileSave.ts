766218ab1a4d3b29e6d869c2523901231e0a2004
liting <luz.liting@gmail.com>
2023-08-06 21:28:10 +0800

    refactor(/editor): rm file-saver

--- composables/useFileSave.ts
+++ composables/useFileSave.ts
@@ -1,28 +1,38 @@
-import { saveAs } from 'file-saver'
-
 export function useFileSave() {
+  const save = (name: string, data: Blob) => {
+    const url = URL.createObjectURL(data)
+    const a = document.createElement('a')
+    a.href = url
+    a.download = name
+    document.body.appendChild(a)
+    a.click()
+    document.body.removeChild(a)
+    URL.revokeObjectURL(url)
+  }
+
   const saveHtml = (name: string, data: string) => {
     const blob = new Blob([data], {
       type: 'text/html',
     })
-    saveAs(blob, `${name}.html`)
+    save(`${name}.html`, blob)
   }
 
   const saveJson = (name: string, data: string) => {
     const blob = new Blob([data], {
       type: 'application/json',
     })
-    saveAs(blob, `${name}.json`)
+    save(`${name}.json`, blob)
   }
 
   const saveText = (name: string, data: string) => {
     const blob = new Blob([data], {
       type: 'text/plain',
     })
-    saveAs(blob, `${name}.txt`)
+    save(`${name}.txt`, blob)
   }
 
   return {
+    save,
     saveHtml,
     saveJson,
     saveText,
