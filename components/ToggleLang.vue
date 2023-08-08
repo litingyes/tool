2b7b96f901c060513c87686310a568b20146b0ea
liting <luz.liting@gmail.com>
2023-08-09 00:29:39 +0800

    perf(/editor): toggle lang style

--- components/ToggleLang.vue
+++ components/ToggleLang.vue
@@ -1,26 +1,34 @@
 <script lang="ts" setup>
 const { locale } = useI18n()
+
+const langMenu = [
+  [
+    {
+      id: 'en',
+      label: 'English',
+      click: () => {
+        locale.value = 'en'
+      },
+    },
+  ],
+  [
+    {
+      id: 'zh',
+      label: '简体中文',
+      click: () => {
+        locale.value = 'zh'
+      },
+    },
+  ],
+]
 </script>
 
 <template>
-  <UPopover>
+  <UDropdown :items="langMenu" :ui="{ width: 'w-24' }">
     <UButton icon="i-material-symbols-language" size="sm" variant="ghost" square />
 
-    <template #panel>
-      <ul class="w-20 text-sm">
-        <li
-          class="cursor-pointer px-2 py-1 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700"
-          :class="[locale === 'en' ? 'text-primary-600 dark:text-primary-400' : 'hover:text-primary-600 hover:dark:text-primary-300']" @click="locale = 'en'"
-        >
-          English
-        </li>
-        <li
-          class="cursor-pointer px-2 py-1 transition-colors hover:bg-gray-100 dark:hover:bg-gray-700"
-          :class="[locale === 'zh' ? 'text-primary-600 dark:text-primary-400' : 'hover:text-primary-600 hover:dark:text-primary-300']" @click="locale = 'zh'"
-        >
-          简体中文
-        </li>
-      </ul>
+    <template #item="{ item }">
+      <span :class="{ 'text-primary-600 dark:text-primary-400': item.id === locale }">{{ item.label }}</span>
     </template>
-  </UPopover>
+  </UDropdown>
 </template>
